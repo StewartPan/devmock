0499c948358cb20287adec607c43dad2cd2c5cd4
zhpan <zhpan@microstrategy.com>
2018-06-25 08:03:29 -0400

    commit for homework

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,33 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
  	record = require('../lib/record');
- 	url = require('url');
 // Create a proxy server with custom application logic
 //
-var proxy = httpProxy.createProxyServer({});
- 
+
+var option = {
+  target: 'http://localhost:9005',
+  selfHandleResponse : true
+};
+
+var proxy = httpProxy.createProxyServer(option);
+
+proxy.listen(5050);
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  var body = new Buffer('');
+  proxyRes.on('data', function (data) {
+    body = Buffer.concat([body, data]);
+  });
+    proxyRes.on('end', function () {
+    body = body.toString();
+    //console.log("res from proxied server:", body);
+    let storePath = '../data/recordData' + req.url;
+    console.log(storePath);
+    //record(storePath, body);
+    res.end("my response to cli");
+    });
+});
+
 //
 // Create your custom server and just call `proxy.web()` to proxy
 // a web request to the target passed in the options
@@ -13,29 +35,13 @@ var proxy = httpProxy.createProxyServer({});
 //
 
 
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-    var option = {
-      target: 'http://localhost:9005',
-      selfHandleResponse : true
-    };
-    proxy.on('proxyRes', function (proxyRes, req, res) {
-      var body = new Buffer('');
-      proxyRes.on('data', function (data) {
-        body = Buffer.concat([body, data]);
-      });
-        proxyRes.on('end', function () {
-        body = body.toString();
-        //console.log("res from proxied server:", body);
-        let storePath = '../data/recordData' + req.url;
-        console.log(storePath);
-        record(storePath, body);
-        res.end("my response to cli");
-        });
-    });
-    proxy.web(req, res, option);
-});
- 
+// var server = http.createServer(function(req, res) {
+//   // You can define here your custom logic to handle the request
+//   // and then proxy the request.
+//
+//
+//     proxy.web(req, res, option);
+// });
+
 console.log("listening on port 5050")
-server.listen(5050);
+// server.listen(5050);
