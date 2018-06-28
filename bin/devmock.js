8f89de7a05971704a0d9e1164661d71a8c0c0fc4
Ma, Jianglong <jima@microstrategy.com>
2018-06-28 17:43:00 +0800

    add prompt

--- bin/devmock.js
+++ bin/devmock.js
@@ -1,26 +1,62 @@
 const argv = require('argv');
 const package = require('../package');
 const devmock = require('../lib/main');
+const runPrompt = require('./prompt');
 
-const SERVER_MOD = {
+const MODS = [{
+  mod: 'init',
+  description: 'Initialize a config file',
+  options: [{
+    name: 'yes',
+    short: 'y',
+    type: 'boolean'
+  }]
+}, {
   mod: 'server',
   description: 'Http mock server',
   options: [{
     name: 'port',
     short: 'p',
     type: 'int'
-  },{
+  }, {
     name: 'dir',
     short: 'd',
     type: 'path'
   }]
+}];
+
+const schema = {
+  properties: {
+    server_port: {
+      description: 'Enter server port',
+      type: 'integer',
+      default: 9080,
+      required: true
+    },
+    server_url: {
+      description: 'Enter server url to proxy',
+      type: 'string',
+      default: 'http://localhost:8080',
+      required: true
+    }
+  }
 };
 
 // parse command-line arguments
-let {options, mod} = argv.mod(SERVER_MOD).version(package.version).run();
+MODS.forEach((opt) => argv.mod(opt));
+let {options, mod} = argv.version(package.version).run();
 
-if (mod == 'server') {
+if (mod == 'init') {
+  if (options.yes) { // Use default config
+    devmock.initConfigFile();
+  }
+  else { // Use prompt
+    runPrompt(schema,config=>devmock.initConfigFile(config));
+  }
+}
+else if (mod == 'server') {
   devmock.startServer(options);
-} else {
+}
+else if (mod == 'ws') {
   devmock.startWS(options);
 }
---
+++ bin/prompt.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt');
+
+module.exports = function runPrompt(schema, cb) {
+  prompt.start();
+  prompt.message = '';
+  prompt.get(schema, function (err, result) {
+    if(err){
+      console.error(err);
+    }
+    else {
+      cb && cb(result);
+    }
+  });
+};
+
