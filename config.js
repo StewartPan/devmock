720f4e65af38f4727dd34f9f1048a62e618e0565
zhpan <panzw1105@gmail.com>
2018-07-11 03:09:33 -0400

    add regular expression matching and write to file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  server_port: 9080,
+  websocket_port: 9090,
+  server_url: 'http://localhost:8080',
+  target_url: '/MstrMainBranch/servlet/mstrWeb',
+  distinguisher: '',
+  routes: [
+  ]
+}
--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,9 @@
-var fs = require('fs'),
-    util = require('util'),
-    cp = require('child_process');
+const fs = require('fs');
 
-var filename = 'jsonTest.js';
-var lines2nuke = 3;
-var command = util.format('tail -n %d %s', lines2nuke, filename);
+let regex = new RegExp("/(route:)//$]/");
 
-cp.exec(command, (err, stdout, stderr) => {
+fs.readFile("jsonTest.js", 'utf8', function (err, data) {
     if (err) throw err;
-    var to_vanquish = stdout.length;
-    console.log('stdout length is ', stdout.length);
-    fs.stat(filename, (err, stats) => {
-        if (err) throw err;
-        fs.truncate(filename, stats.size - to_vanquish, (err) => {
-            if (err) throw err;
-            console.log('File truncated!');
-        })
-    });
+    console.log(data);
+    console.log('Res is ', data.replace(regex, 'Oh my god'));
 });
