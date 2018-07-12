5ef48b28765212583d560f74c885f000be682655
zhpan <panzw1105@gmail.com>
2018-07-12 10:23:38 -0400

    take distingquisher into count

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,33 @@
-const fs = require('fs');
-
-let regex = new RegExp("/(route:)//$]/");
-
-fs.readFile("jsonTest.js", 'utf8', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-    console.log('Res is ', data.replace(regex, 'Oh my god'));
-});
+var http = require('http');
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // BEGINNING OF NEW STUFF
+    response.on('error', function(err) {
+      console.error(err);
+    });
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // 注：上面两行代码可以用下面一行替换
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // 注：同样，可以这样替换
+    // response.end(JSON.stringify(responseBody))
+    // END OF NEW STUFF
+  });
+}).listen(9090);
