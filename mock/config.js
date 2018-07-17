39348f11b908733e24669e012dd883fcb69adf0f
zhpan <panzw1105@gmail.com>
2018-07-17 08:28:34 -0400

    work function fully in post request

--- mock/config.js
+++ mock/config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  server_port: 9998,
+  server_port: 9080,
   ws_port: 9090,
   server_url: 'http://localhost:3000',
-  target_url: '/MSTR/',
-  distinguisher: '',
-  routes: [ //"G",
-    //"G",
-    //"G",
-    //"G",
+  distinguisher: [],
+  target_url: '',
+  routes: [
   ]
 }
