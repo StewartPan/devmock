9b09fe52fb74376414f2953662c0675cc5bcff6d
zhpan <panzw1105@gmail.com>
2018-07-18 05:03:31 -0400

    commit before change req body

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,9 @@
-const path = require('path');
-let test = require('./a.json');
-console.log(path.normalize('/MSTR'));
+const http = require('http');
+const httpProxy = require('http-proxy');
+let proxy = httpProxy.createProxyServer({
+  target: 'http://localhost:8080',
+  auth: 'user:password'
+});
+let server = http.createServer(function(req, res){
+  proxy.web(req, res);
+}).listen(9081);
