ecb4bf3a44688f65412451f8824a6ece1c94913b
zhpan <panzw1105@gmail.com>
2018-07-23 06:22:04 -0400

    recalculate content-length of recorded data after modified

--- mockData/config.js
+++ mockData/config.js
@@ -5,5 +5,14 @@ module.exports = {
   distinguisher: ['taskId'],
   target_url: '/MstrMainBranch/servlet/taskProc',
   routes: [
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\keepSessionAlive.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\getLocaleInfo.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\getBundleDescriptors.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\qBuilder.GetQuota.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\arch.search.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\getDIProjectSettings.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\getMapShapes.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\arch.getCustomConnector.json",
+    //"POST\\MstrMainBranch\\servlet\\taskProc\\arch.getDBObjects.json",
   ]
 }
