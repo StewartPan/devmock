56f2a740ffdeaab9f6fdc31cef4d3be515e4ebf4
zhpan <panzw1105@gmail.com>
2018-07-26 15:03:52 +0800

    change content-length and server info logger

---
+++ mockdata/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  server_port: 9080,
+  ws_port: 9090,
+  server_url: 'http://localhost:8080',
+  distinguisher: ['taskId'],
+  target_url: 'MstrMainBranch/servlet/taskProc',
+  recordLatest: true,
+  routes: [
+  ]
+}
