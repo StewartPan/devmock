6146d9c4fef29b4fb9e5f10787323167e2fd1fe7
StewartPan <panzw1105@gmail.com>
2018-08-16 03:51:24 -0400

    add mstrweb in filter and use promised to rewrite compressData

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # devmock
 A mock server for web development which decouple frontend and backend developers and improve development efficiency.
 
-**This is test version**
+> Record, mock, and proxy HTTP/HTTPS traffic as middleware for the connect middleware framework.
+
+## Getting Started
+
+devmock can be installed via npm with the following command.
+
+```shell
+npm install devmock --save-dev
+```
+
+## Overview
+
+devmock adopts idea from the Ruby project [VCR](https://github.com/elcuervo/vcr.js).
--- tests/test.js
+++ tests/test.js
@@ -14,4 +14,28 @@
 //
 // console.log("listening on port 5050");
 // server.listen(5050);
+const zlib = require('zlib');
 
+
+function compressData (dataStr, encodingType) {
+    return new Promise((resolve, reject) => {
+        if (encodingType == 'gzip') {
+            zlib.gzip(dataStr, function (error, result) {
+                if (error) throw error;
+                resolve(result);
+            });
+        } else if (encodingType == 'deflate') {
+            zlib.deflate(dataStr, function (error, result) {
+                if (error) throw error;
+                resolve(result);
+            });
+        } else {
+            resolve(result);
+        }
+    });
+}
+
+compressData("haha", 'gzip').then((value) => {
+        console.log('value is ', value);
+    }
+);
